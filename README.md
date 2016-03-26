69f7f4c790e1466f90c713869c2e22c04637872a
CH.Idea <sbw228@gmail.com>
2016-03-26 16:15:33 +0900

    done

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # GoPythonDLLWrapper
 A Go program that wraps python dll and scripts to ease up the python program distribution for windows.
+
+### Possible application
+  - Multiple Python interpreters loaded by Go routines
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime"
 	"syscall"
 	"time"
 	"unsafe"
@@ -19,35 +20,43 @@ var (
 	Run         = py.NewProc("PyRun_SimpleString")
 	Main        = py.NewProc("Py_Main")
 	Free        = py.NewProc("PyMem_RawFree")
+	RawMalloc   = py.NewProc("PyMem_RawMalloc")
+	Decode      = py.NewProc("Py_DecodeLocale")
 )
 
 func main() {
-	//r, _, _ := SetPath.Call(
-	//uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(""))))
-	prog := uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr("python")))
-	Call(SetProgName, prog)
-	//uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(path))))
+	runtime.GOMAXPROCS(runtime.NumCPU()) // use all physical cores
+
+	//prog := uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr("python")))
+	//Call(SetProgName, prog)
 	Call(Init)
-	argc := len(os.Args) - 1
-	argv := make([]*uint16, argc+1)
+	argc := len(os.Args)
+	argv := make([]uintptr, argc)
 	for i, v := range os.Args[1:] {
-		argv[i] = syscall.StringToUTF16Ptr(v)
+		argv[i+1] = uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(v)))
 	}
-	argv[argc] = nil
-	//Call(Main, uintptr(argc), uintptr(unsafe.Pointer(&argv)))
 	Call(SetArgvEx, uintptr(unsafe.Pointer(&argv[0])), uintptr(argc), uintptr(0))
-	//Call(SetArgv, uintptr(unsafe.Pointer(&argv[0])), uintptr(unsafe.Pointer(&argc)))
+	//Call(SetArgv, uintptr(unsafe.Pointer(&argv[0])), uintptr(unsafe.Pointer(&argc))) // May needed for older python versions
+	fmt.Println("Running", os.Args[1], "...")
+	Call(Main, uintptr(argc), uintptr(unsafe.Pointer(&argv[0])))
 	//updatepath := 0
-	//fmt.Println("Running", os.Args[1])
-	script := syscall.StringByteSlice(`from sys import argv; print(argv)`)
+
+	/* Example : inline script instead of external script execution */
+	//script := syscall.StringByteSlice(`from sys import argv; print(argv)`)
 	//script := syscall.StringByteSlice(`import sys; sys.argv=['idea.sh']; from imp import load_module, PY_SOURCE; f='main.py'; load_module('__main__', open(f), f, ('.py', 'r', PY_SOURCE))`)
-	Call(Run, uintptr(unsafe.Pointer(&script[0])))
+	//Call(Run, uintptr(unsafe.Pointer(&script[0])))
+
 	Call(Finalize)
 	time.Sleep(1 * time.Microsecond)
-	Call(Free, prog)
+	//Call(Free, prog)
+	for _, v := range argv {
+		Call(Free, v)
+	}
 }
 
-func Call(proc *syscall.LazyProc, args ...uintptr) {
+func Call(proc *syscall.LazyProc, args ...uintptr) uintptr {
 	r1, r2, le := proc.Call(args...)
-	fmt.Println(r1, r2, le)
+	//fmt.Println(r1, r2, le) // Debug purpose
+	_, _ = r2, le
+	return r1
 }
